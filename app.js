636c7deb11be948306ee983de3a4b7f662a2968a
Oumar <baarryoumar@gmail.com>
2023-01-17 23:22:30 -0500

    challenge 11 completed

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+var posts = [];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,6 +23,7 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
 
   res.render('home', {startingContent:homeStartingContent});
+  console.log(posts);
 
 });
 
@@ -45,26 +48,14 @@ app.get('/compose', (req, res) => {
 
 app.post('/compose', (req, res) => {
 
-  let post = {
-
+  const post = {
     Title:req.body.postTitle,
     Body:req.body.postBody
-  }
+  };
   
+  posts.push(post);
+  res.redirect('/');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
